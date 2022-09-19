6a8d890054283c673ca68c7693cea19c2975d9a2
qiguang <qiguang0120@163.com>
2022-09-19 11:06:11 +0800

    Initial commit

---
+++ src/main/java/com/example/javarealm/JavaRealmApplication.java
@@ -0,0 +1,13 @@
+package com.example.javarealm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRealmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaRealmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javarealm/JavaRealmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javarealm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRealmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
