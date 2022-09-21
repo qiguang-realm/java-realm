9458e8e91cfaf1c12facfb832893234217d7fb2a
qiguang <qiguang0120@163.com>
2022-09-21 17:36:06 +0800

    Informal Essay

---
+++ java-documentation/src/main/java/pers/qiguang/documentation/onjava8/InformalEssay.java
@@ -0,0 +1,19 @@
+package pers.qiguang.documentation.onjava8;
+
+/**
+ * @author qig
+ * @since 2022-09-20
+ **/
+public class InformalEssay {
+
+    // 代码注释的艺术，优秀代码真的不需要注释吗？
+
+    // 理论与实践：如何写好一个方法
+
+    // Code Review 指南  https://blog.csdn.net/itguangit/article/details/123003006
+
+
+
+    //
+
+}
