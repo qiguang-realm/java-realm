219482da8b0b9291dbf0553b8d657922dbde2c6e
qiguang <qiguang0120@163.com>
2025-01-10 15:02:17 +0800

    Initial commit

--- java-documentation/src/main/java/pers/qiguang/documentation/JavaPlatformApplication.java
+++ java-documentation/src/main/java/pers/qiguang/documentation/JavaPlatformApplication.java
@@ -4,14 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * On Java 8
- * https://github.com/BruceEckel/OnJava8-Examples
- *
  * @author qig
- * @since 2022-09-19
  **/
 @SpringBootApplication
-//@MapperScan("pers.qiguang.documentation.mapper")  //@MapperScan 注解，扫描 Mapper 文件夹
 public class JavaPlatformApplication {
     public static void main(String[] args) {
         SpringApplication.run(JavaPlatformApplication.class, args);
