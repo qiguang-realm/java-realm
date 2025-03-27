c6559cfade3542332be3fe49fb0f12e8a86ce875
qiguang <qiguang0120@163.com>
2025-03-27 17:32:42 +0800

    vue-element-admin

---
+++ realm-gateway/src/main/java/cn/realm/cloud/gateway/GatewayServerApplication.java
@@ -0,0 +1,17 @@
+package cn.realm.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qig
+ */
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+        System.out.println("(*^▽^*)启动成功!!!(〃'▽'〃)");
+    }
+
+}
--- realm-module-system/realm-module-system-biz/src/main/java/cn/realm/cloud/module/system/SystemServerApplication.java
+++ realm-module-system/realm-module-system-biz/src/main/java/cn/realm/cloud/module/system/SystemServerApplication.java
@@ -1,14 +1,14 @@
-package pers.qiguang.documentation;
+package cn.realm.cloud.module.system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author qig
- **/
+ */
 @SpringBootApplication
-public class JavaPlatformApplication {
+public class SystemServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JavaPlatformApplication.class, args);
+        SpringApplication.run(SystemServerApplication.class, args);
     }
 }
